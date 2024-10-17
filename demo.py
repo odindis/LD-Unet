52b4d5a2eae9844471cf9d48fbce98fdb18f493d
UULE <32983665+odindis@users.noreply.github.com>
2024-10-17 10:33:33 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,9 +6,14 @@ the input data to 3D.
   
 import model 
 import torch
-  
-model = model.LD_UNet( in_channel=1, out_channel=2 )
-x = torch.randn( 1, 1, 1, 256, 256 )
+
+in_channel = 1
+out_channel = 2
+batch_size = 3
+image_shape = [1,256,256]
+
+model = model.LD_UNet( in_channel=in_channel, out_channel=out_channel )
+x = torch.randn( batch_size, in_channel, image_shape[0], image_shape[1], image_shape[2] )
 y = model( x )
 for i in y:
     print( i.shape )
